8d6fa6c06c2a3116caa706dc4eb4219ab1846647
Ihar Trymailau <ihar.trymailau@gmail.com>
2025-02-01 19:49:33 +0300

    feat: add loader button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from "../components/loader/loader.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'json-vizualizer';
